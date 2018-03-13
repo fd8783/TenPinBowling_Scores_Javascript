b49086748f4ea9ac341f50c2e8f343955c976f79
fd8783 <30581760+fd8783@users.noreply.github.com>
2018-03-13 19:12:28 +0800

    OK

--- TenPinBowling_Scores_Javascript.js
+++ TenPinBowling_Scores_Javascript.js
@@ -1,4 +1,48 @@
 
 function Calculate(input){
-	return input;
+	var inputList = input.split(',');
+	var scoreList = inputList.map(function (score) { 
+		return parseInt(score, 10); 
+	});
+
+	var totalScore = 0,
+		turn = 0;
+		
+
+	for (i=0; i < scoreList.length && turn < 10; i++){
+		if (scoreList[i] == 10){
+			turn++;
+			switch (scoreList.length - 1 - i){
+				case 0:
+					totalScore += scoreList[i];
+					break;
+				case 1:
+					totalScore += scoreList[i]+scoreList[i+1];
+					break;
+				default:
+					totalScore += scoreList[i]+scoreList[i+1]+scoreList[i+2];
+					break;
+			}
+			
+		}
+		else{
+			turn+=0.5;
+			if (turn%1 == 0){
+				if (scoreList[i-1]+scoreList[i] == 10){
+					if ((scoreList.length - 1 - i) == 0)
+						totalScore += scoreList[i];
+					else
+						totalScore += scoreList[i]+scoreList[i+1];
+				}
+				else{
+					totalScore += scoreList[i];
+				}
+			}
+			else{
+				totalScore += scoreList[i];
+			}
+		}
+	}
+	
+	return totalScore;
 }
